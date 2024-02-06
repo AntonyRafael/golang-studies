5096c20e2b68476a4c85f7d23767a3392989fe93
Antony Rafael <antony.costa@mercadolivre.com>
2024-02-06 20:34:32 -0300

    create seguidores table

--- devbook/api/sql/sql.sql
+++ devbook/api/sql/sql.sql
@@ -12,3 +12,14 @@ CREATE TABLE usuarios (
     senha VARCHAR(120) NOT NULL,
     criadoEm  timestamp default current_timestamp()
 ) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS seguidores;
+
+CREATE TABLE seguidores (
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    seguidor_id INT NOT NULL,
+    FOREIGN KEY (seguidor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+
+    PRIMARY KEY (usuario_id, seguidor_id)
+) ENGINE=InnoDB;
