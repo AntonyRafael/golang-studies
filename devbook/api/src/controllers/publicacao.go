bfee9345c82833c9632851c431c8482347f0e971
Antony Rafael <antony.costa@mercadolivre.com>
2024-02-14 20:40:30 -0300

    create basic structure for publicacoes routes

---
+++ devbook/api/src/controllers/publicacao.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao cria uma nova publicação
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacoes busca todas as publicações do usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacao busca uma publicação específica do usuário
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// AtualizarPublicacao atualiza uma publicação do usuário
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletarPublicacao deleta uma publicação do usuário
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
