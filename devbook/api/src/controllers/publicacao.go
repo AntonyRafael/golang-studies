472c88a2a51644de7235e8fc90324188fdf527c5
Antony Rafael <antony.costa@mercadolivre.com>
2024-02-14 20:56:56 -0300

    create post route

--- devbook/api/src/controllers/publicacao.go
+++ devbook/api/src/controllers/publicacao.go
@@ -1,10 +1,56 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // CriarPublicacao cria uma nova publicação
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	corpoDaRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoDaRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, err := banco.Conectar()
+
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
 }
 
 // BuscarPublicacoes busca todas as publicações do usuário
