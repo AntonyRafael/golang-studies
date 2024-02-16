dcc09c378b436e8313d210ccfa502104863dd838
Antony Rafael <antony.costa@mercadolivre.com>
2024-02-15 21:07:42 -0300

    deslike post route

--- devbook/api/src/repositorios/publicacoes.go
+++ devbook/api/src/repositorios/publicacoes.go
@@ -194,3 +194,22 @@ func (repositorio Publicacoes) Curtir(publicacaoID uint64) error {
 
 	return nil
 }
+
+// Descurtir remove uma curtida na publicação
+func (repositorio Publicacoes) Descurtir(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare(`
+		UPDATE publicacoes SET curtidas = 
+		CASE WHEN curtidas > 0 THEN curtidas -1
+		ELSE curtidas END
+		where id = ?`)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
