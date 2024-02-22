05bb7f3085543aea220c0dd21d2f8be66b14531a
Antony Rafael <antony.costa@mercadolivre.com>
2024-02-21 21:19:19 -0300

    basic structure login page

--- devbook/webapp/src/router/router.go
+++ devbook/webapp/src/router/router.go
@@ -1,9 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"webapp/src/router/rotas"
+
+	"github.com/gorilla/mux"
+)
 
 // Gerar retorna um router com as rotas configuradas
 func Gerar() *mux.Router {
 	r := mux.NewRouter()
-	return r
+	return rotas.Configurar(r)
 }
